81e8900d0ef358c5957cd08e6fbecd4106d4ee25
João Marcos <jmarcosmmn@gmail.com>
2023-10-06 01:15:13 -0300

    update website

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ var content = document.querySelector(".study-content");
 
 function trocarcor(idprincipal) {
     document.getElementById(idprincipal).style.backgroundImage = "none";
-    document.getElementById(idprincipal).style.backgroundColor = "#ffffff";
+    document.getElementById(idprincipal).style.borderBottom = "5px #DBC816 solid";
 }
 
 function voltarcor(id1, id2, id3) {
-    document.getElementById(id1).style.backgroundColor = "#181818";
-    document.getElementById(id2).style.backgroundColor = "#181818";
-    document.getElementById(id3).style.backgroundColor = "#181818";
+    document.getElementById(id1).style.border= "none";
+    document.getElementById(id2).style.border= "none";
+    document.getElementById(id3).style.border= "none";
 }
 
 uninter.addEventListener("click", function() {
@@ -34,7 +34,7 @@ programacaoDoZero.addEventListener("click", function() {
 });
 
 santanderBootcamp.addEventListener("click", function() {
-    content.innerHTML = "<h2>Ciência de dados com Python - Santander Bootcamp [89 horas]</h2><p>Bootcamp da Dio.me em parceria com Santander. Aprendizado da linguagem Python e suas aplicações, bancos de dados SQL e NoSQL, visualização e análise de dados com Power BI, e fundamentos e técnicas de machine learning.</p><a target=\"_blank\" href=\"https://drive.google.com/drive/folders/1oYUV0IY-b5kzrOidD8HSV8gdVusctzMm?usp=sharing\"></a><p><mark>Python</mark> <mark>SQL e No SQL</mark> <mark>Power BI</mark> <mark>Machine Learning</mark></p><button class=\"certificacoes\">Certificações</button>";
+    content.innerHTML = "<h2>Ciência de dados com Python - Santander Bootcamp [89 horas]</h2><p>Bootcamp da Dio.me em parceria com Santander. Aprendizado da linguagem Python e suas aplicações, bancos de dados SQL e NoSQL, visualização e análise de dados com Power BI, e fundamentos e técnicas de machine learning.</p><a target=\"_blank\" href=\"https://drive.google.com/drive/folders/1oYUV0IY-b5kzrOidD8HSV8gdVusctzMm?usp=sharing\"></a><p><mark>Python</mark> <mark>SQL e No SQL</mark> <mark>Power BI</mark> <mark>Machine Learning</mark></p><a target=\"_blank\" href=\"https://drive.google.com/drive/folders/1oYUV0IY-b5kzrOidD8HSV8gdVusctzMm?usp=sharing\"><button class=\"certificacoes\">Certificações</button></a>";
     trocarcor("santander-bootcamp");
     voltarcor("python", "programacao-do-zero", "uninter")
 });
@@ -61,21 +61,21 @@ function voltarCorProject(idproject1, idproject2, idproject3) {
 }
 
 projeto1.addEventListener("click", function() {
-    contentProject.innerHTML = "<a target=\"blank\" href=\"images/gif-portifolio.gif\"><img align=\"right\" src=\"images/gif-portifolio.gif\"/></a><h2>Este portifólio</h2><p>Este portifólio teve seu início na minha imaginação. Para torná-lo autêntico, o objetivo foi evitar o uso de modelos prontos ou outros sites como referência. Partindo apenas de um esboço criado no meu computador, desenvolvi tanto a estrutura quanto o design a partir do zero, utilizando HTML, CSS e JavaScript puros. Ainda tenho muitas melhorias a fazer nele e estou atualizando-o de acordo com o meu progresso nas tecnologias. Tentei aplicar a responsividade usando divs em grade e flexíveis, além de utilizar a media query no CSS.</p><div class=\"buttons\"><img src=\"images/github-mark.png\"/><img src=\"images/link-externo.png\"/></div>";
+    contentProject.innerHTML = "<a target=\"blank\" href=\"images/gif-portifolio.gif\"><img align=\"right\" src=\"images/gif-portifolio.gif\"/></a><h2>Este portifólio</h2><p>Este portifólio teve seu início na minha imaginação. Para torná-lo autêntico, o objetivo foi evitar o uso de modelos prontos ou outros sites como referência. Partindo apenas de um esboço criado no meu computador, desenvolvi tanto a estrutura quanto o design a partir do zero, utilizando HTML, CSS e JavaScript puros. Ainda tenho muitas melhorias a fazer nele e estou atualizando-o conforme meu progresso de aprendizado nas tecnologias. Tentei aplicar a responsividade usando divs em grade e flexíveis, além de utilizar media query no CSS.</p><div class=\"buttons\"><a target=\"_blank\" href=\"https://github.com/jmarcosbs/portfolio\"><img src=\"images/github-mark.png\"/></a><img style=\"filter: grayscale(100%); opacity: 0.7;\" src=\"images/link-externo.png\"/></div>";
     /// trocarCorProject("projeto1");
     /// voltarCorProject("projeto2", "projeto3", "projeto4")
     /// voltarCorFontProject("projeto2", "projeto3", "projeto4")
 });
 
 projeto2.addEventListener("click", function() {
-    contentProject.innerHTML = "<a target=\"blank\" href=\"images/images/regioes.png\"><img align=\"right\" src=\"images/regioes.png\"/></a><h2>Indicador de Análise Gráfica</h2><p>Este foi meu primeiro projeto autêntico em Python, um indicador que procura áreas de congestão nos gráficos de preços. Estou estudando para adicionar mais recursos e técnicas de machine learning no futuro. O objetivo final é migrar esse indicador para as plataformas Metatrader e TradingView. O algoritmo compara metades do intervalo de preços, seleciona a que tem maior pontuação e continua repetindo o processo até não haver mais distância entre as regiões.</p><div class=\"buttons\"><img src=\"images/github-mark.png\"/><img src=\"images/link-externo.png\"/></div>";
+    contentProject.innerHTML = "<a target=\"blank\" href=\"images/images/regioes.png\"><img align=\"right\" src=\"images/regioes.png\"/></a><h2>Indicador de Análise Gráfica</h2><p>Este foi meu primeiro projeto autêntico em Python, um indicador que procura áreas de congestão nos gráficos de preços. Estou estudando para adicionar mais recursos e técnicas de machine learning no futuro. O objetivo final é migrar esse indicador para as plataformas Metatrader e TradingView. O algoritmo compara metades do intervalo de preços, seleciona a que tem maior pontuação e continua repetindo o processo até não haver mais distância entre as regiões.</p><div class=\"buttons\"><a target=\"_blank\" href=\"https://github.com/jmarcosbs/regiao_de_congestao\"><img src=\"images/github-mark.png\"/></a><a target=\"_blank\" href=\"https://colab.research.google.com/github/jmarcosbs/regiao_de_congestao/blob/main/regiao_de_congestao.ipynb\"><img src=\"images/link-externo.png\"/></a></div>";
     /// trocarCorProject("projeto2");
     /// voltarCorProject("projeto1", "projeto3", "projeto4")
     /// voltarFontCorProject("projeto1", "projeto3", "projeto4")
 });
 
 projeto3.addEventListener("click", function() {
-    contentProject.innerHTML = "<a target=\"blank\" href=\"images/etl.gif\"><img align=\"right\" src=\"images/etl.gif\"/></a><h2>ETL para alteração de usuários de API</h2><p>Este projeto teve como objetivo realizar um processo ETL (Extração, Transformação e Carregamento) de dados de uma API pública. O algoritmo extrai informações da API, gera uma lista de usuários com base em um arquivo .csv e permite ao usuário do código identificar o ID do usuário na lista, alterar o nome e, em seguida, encaminhar as atualizações de volta para a API.</p><div class=\"buttons\"><img src=\"images/github-mark.png\"/><img src=\"images/link-externo.png\"/></div>";
+    contentProject.innerHTML = "<a target=\"blank\" href=\"images/etl.gif\"><img align=\"right\" src=\"images/etl.gif\"/></a><h2>ETL para alteração de usuários de API</h2><p>Este projeto teve como objetivo realizar um processo ETL (Extração, Transformação e Carregamento) de dados de uma API pública. O algoritmo extrai informações da API, gera uma lista de usuários com base em um arquivo .csv e permite ao usuário do código identificar o ID do usuário na lista, alterar o nome e, em seguida, encaminhar as atualizações de volta para a API.</p><div class=\"buttons\"><a target=\"_blank\" href=\"https://github.com/jmarcosbs/etl-de-usuarios-em-api\"><img src=\"images/github-mark.png\"/></a><a target=\"_blank\" href=\"https://colab.research.google.com/github/jmarcosbs/etl-de-usuarios-em-api/blob/main/Realizando_ETL_para_alterar_usu%C3%A1rios_em_API.ipynb#scrollTo=kNuP0SDUZMBY\"><img src=\"images/link-externo.png\"/></a></div>";
     /// trocarCorProject("projeto3");
     /// voltarCorProject("projeto2", "projeto1", "projeto4")
     /// voltarCorFontProject("projeto2", "projeto1", "projeto4")
